649e8d588b86d528458f6631e65e213824761d0a
agnieszka8rogowska <62265152+agnieszka8rogowska@users.noreply.github.com>
2020-05-04 23:33:08 +0200

    Add files via upload

--- htdocs/views/index.view.php
+++ htdocs/views/index.view.php
@@ -1,1 +1,100 @@
-.
+<html>
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+
+<a href="insert.php">insert</a>
+<div class="w-75 mx-auto">
+    <div class="w-50 float-left">
+        <div class="w-100 btn btn-primary active">
+            Obiektowo-relacyjna baza danych
+        </div><br><br><br><br><br>
+        <div class="w-75 mx-auto">
+            <form>
+                <div class="form-group">
+                    <button type="submit" class=" btn btn-primary w-100">Odczyt danych</button><br><br><br>
+                    <button type="submit" class="btn btn-primary w-100">Dodawanie danych</button><br><br><br>
+                    <button type="submit" class="btn btn-primary w-100">Modyfikacja danych</button><br><br><br>
+                    <button type="submit" class="btn btn-primary w-100">Usuwanie danych</button><br><br><br>
+                </div><br><br>
+            </form>
+        </div>
+    </div>
+    <div class="w-50 float-right">
+        <div class="w-100 btn btn-secondary active">
+            Relacyjna baza danych
+        </div><br><br><br><br><br>
+        <div class="w-75 mx-auto">
+            <form>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-secondary w-100">Odczyt danych</button><br><br><br>
+                    <button type="submit" class="btn btn-secondary w-100">Dodawanie danych</button><br><br><br>
+                    <button type="submit" class="btn btn-secondary w-100">Modyfikacja danych</button><br><br><br>
+                    <button type="submit" class="btn btn-secondary w-100">Usuwanie danych</button><br><br><br>
+                </div><br><br>
+            </form>
+            </div>
+    </div>
+</div>
+
+
+<div class="w-25 mx-auto"> <h5> Poniższe formularze zostaną wykorzystane do dodawania danych</h5>
+<form action="#" method="get">
+  <div class="form-group">
+    <h5> Podaj boki prostokąta</h5>
+    <label for="prostokat1">Podaj długość pierwszego boku</label>
+    <input type="number" class="form-control" id="prostokat1" placeholder="np. 12">
+    <label for="prostokat2">Podaj długość drugiego boku</label>
+    <input type="number" class="form-control" id="prostokat2" placeholder="np. 10">
+    <button type="submit" class="btn btn-primary float-right">Dodaj</button>
+  </div><br><br>
+  <div class="form-group">
+    <h5> Podaj bok kwadratu</h5>
+    <label for="kwadrat">Podaj długość boku</label>
+    <input type="number" class="form-control" id="kwadrat" placeholder="np. 12">
+    <button type="submit" class="btn btn-primary float-right">Dodaj</button>
+  </div><br><br>
+  <div class="form-group">
+    <h5> Podaj bok pięciokąta</h5>
+    <label for="pieciokat">Podaj długość boku</label>
+    <input type="number" class="form-control" id="pieciokat" placeholder="np. 12">
+    <button type="submit" class="btn btn-primary float-right">Dodaj</button>
+  </div><br><br>
+  <div class="form-group">
+    <h5> Podaj bok sześciokąta</h5>
+    <label for="szesciokat">Podaj długość boku</label>
+    <input type="number" class="form-control" id="szesciokat" placeholder="np. 12">
+    <button type="submit" class="btn btn-primary float-right">Dodaj</button>
+  </div><br><br>
+  <div class="form-group">
+    <h5> Podaj boki trójkąta</h5>
+    <label for="trojkat1">Podaj długość pierwszego boku</label>
+    <input type="number" class="form-control" id="trojkat1" placeholder="np. 12">
+    <label for="trojkat2">Podaj długość drugiego boku</label>
+    <input type="number" class="form-control" id="trojkat2" placeholder="np. 10">
+    <label for="trojkat3">Podaj długość trzeciego boku</label>
+    <input type="number" class="form-control" id="trojkat3" placeholder="np. 11">
+    <button type="submit" class="btn btn-primary float-right">Dodaj</button>
+  </div><br><br>
+  <div class="form-group">
+    <h5> Koło</h5>
+    <label for="kolo">Podaj promień koła</label>
+    <input type="number" class="form-control" id="kolo" placeholder="np. 12">
+    <button type="submit" class="btn btn-primary float-right">Dodaj</button>
+  </div><br><br>
+</form></div>
+
+
+<div class="w-25 mx-auto">
+<form action="addKolo" method="POST">
+<div class="form-group">
+    <h5> KOŁO</h5>
+    <label for="kolo">Podaj promień koła</label>
+    <input type="number" class="form-control" name="kolo" placeholder="np. 12">
+    <button type="submit" class="btn btn-primary float-right">Dodaj</button>
+  </div><br><br>
+</form></div>
+
+</body>
+</html> 
