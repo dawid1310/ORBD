b0df54469177e9ff001bb6ee22c9ccf8bd37272a
agnieszka8rogowska <62265152+agnieszka8rogowska@users.noreply.github.com>
2020-06-01 22:51:30 +0200

    Delete addProstokat.php

--- htdocs/controllers/addProstokat.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$bok1 = $_POST['bok1'];
-$bok2 = $_POST['bok2'];
-$prostokat = new Prostokat();
-$prostokat->getFromUser($bok1, $bok2);
-$prostokat->LoadToDB();
-header("Location: home");
-
