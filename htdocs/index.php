eb82f8f0c2bae181c7223d7ec145fea2792a7c82
agnieszka8rogowska <62265152+agnieszka8rogowska@users.noreply.github.com>
2020-06-15 22:30:29 +0200

    Add files via upload

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,31 +1,7 @@
 <?php
-/*
-$tns = "  
-(DESCRIPTION =
-    (ADDRESS_LIST =
-      (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521))
-    )
-    (CONNECT_DATA =
-      (SERVICE_NAME = orcl)
-    )
-  )
-       ";
-$db_username = "SYSTEM";
-$db_password = "root";
-try{
-    $conn = new PDO("oci:dbname=".$tns,$db_username,$db_password);
-}catch(PDOException $e){
-    echo ($e->getMessage());
-}
-
-*/
-
-
 
 session_start();
 require 'core/bootstrap.php';
-//$a = $app['database']->selectAll('sys.tabela');
-//var_dump($a);
 require Router::load('routes.php')
     ->direct(Request::uri(), Request::method());
     
--- htdocs/routes.php
+++ htdocs/routes.php
@@ -2,17 +2,27 @@
 
 $router->get('' , 'controllers/index.php');
 $router->get('home' , 'controllers/index.php');
-$router->post('addKolo', 'controllers/addKolo.php');
-$router->post('addKwadrat', 'controllers/addKwadrat.php');
-$router->post('addProstokat', 'controllers/addProstokat.php');
-$router->post('addTrojkat', 'controllers/addTrojkat.php');
-$router->post('addPieciokat', 'controllers/addPieciokat.php');
-$router->post('addSzesciokat', 'controllers/addSzesciokat.php');
 
 $router->get('add', 'controllers/add.php');
 $router->post('add', 'controllers/add.php');
-
 $router->get('select', 'controllers/select.php');
 $router->post('select', 'controllers/select.php');
+$router->post('display', 'controllers/display.php');
+$router->get('sfm', 'controllers/selectForModyfication.php');
 $router->get('delete', 'controllers/delete.php');
 $router->post('delete', 'controllers/delete.php');
+$router->get('modify', 'controllers/modify.php');
+$router->post('modify', 'controllers/modify.php');
+$router->post('applyModyfication', 'controllers/applyModyfication.php');
+
+$router->get('addOBJ', 'controllers/addOBJ.php');
+$router->post('addOBJ', 'controllers/addOBJ.php');
+$router->get('selectOBJ', 'controllers/selectOBJ.php');
+$router->post('selectOBJ', 'controllers/selectOBJ.php');
+$router->post('displayOBJ', 'controllers/displayOBJ.php');
+$router->get('deleteOBJ', 'controllers/deleteOBJ.php');
+$router->post('deleteOBJ', 'controllers/deleteOBJ.php');
+$router->get('modifyOBJ', 'controllers/modifyOBJ.php');
+$router->post('modifyOBJ', 'controllers/modifyOBJ.php');
+$router->post('applyModyficationOBJ', 'controllers/applyModyficationOBJ.php');
+$router->get('sfmOBJ', 'controllers/selectForModyficationOBJ.php');
